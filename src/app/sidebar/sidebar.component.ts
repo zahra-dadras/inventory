a27ca4241e81e660a92f1c813281fbd10c185e8f
zahra-dadras <zahradadras77@gmail.com>
2025-04-28 23:24:12 +0330

    add sidebar menu

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,46 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppEnum } from '../enum/app-enum.enum';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  public showClickIcon: boolean = false;
+  public productManagement: boolean = false;
+  public anbarReport: boolean = false;
+  public anbarSetting: boolean = false;
+  public appEnum = AppEnum;
+
+  constructor(
+    private router: Router
+  ) { }
+  protected productBankAction() {
+    this.router.navigate(['/product'])
+  }
+
+  protected storeroomAction(): void {
+    this.router.navigate(['/storeroom-info'])
+  }
+
+  public test() {
+    this.showClickIcon = !this.showClickIcon
+  }
+
+  public productManagementAction(): void {
+    this.productManagement = !this.productManagement;
+  }
+
+  public anbarReportAction(): void {
+    this.anbarReport = !this.anbarReport;
+  }
+
+  public anbarSettingAction(): void {
+    this.anbarSetting = !this.anbarSetting;
+  }
+}
